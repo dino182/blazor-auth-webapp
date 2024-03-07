5e86d10f47143042ed2e4fe227053bfeed1a9817
Dino Infantino <dino.infantino@displayplan.com>
2024-03-07 13:10:40 +0000

    Add distributed token cache so access tokens are shared between both applications

--- BlazorAuth/BlazorAuth/Program.cs
+++ BlazorAuth/BlazorAuth/Program.cs
@@ -22,7 +22,22 @@ builder.Services.AddScoped<WeatherForecaster>();
 builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration)
     .EnableTokenAcquisitionToCallDownstreamApi()
     .AddMicrosoftGraph()
-    .AddInMemoryTokenCaches();
+    .AddDistributedTokenCaches();
+
+// Configure a SQL Server distributed token cache
+// See https://learn.microsoft.com/en-us/aspnet/core/performance/caching/distributed?view=aspnetcore-8.0#distributed-sql-server-cache
+builder.Services.AddDistributedSqlServerCache(options =>
+{
+    options.ConnectionString = builder.Configuration.GetConnectionString("Auth");
+    options.SchemaName = "dbo";
+    options.TableName = "TokenCache";
+
+    // You don't want the SQL token cache to be purged before the access token has expired. Usually
+    // access tokens expire after 1 hours (but this can be changed by token lifetime policies), whereas
+    // the default sliding expiration for the distributed SQL database is 20 mins. 
+    // Use a value which is above 60 mins (or the lifetime of a token in case of longer lived tokens)
+    options.DefaultSlidingExpiration = TimeSpan.FromMinutes(90);
+});
 
 // Set the authentication cookie name the same value as the fallback application
 builder.Services.Configure<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme, options =>
--- LegacyWebApplication/App_Start/Startup.Auth.cs
+++ LegacyWebApplication/App_Start/Startup.Auth.cs
@@ -2,10 +2,11 @@
 using System.Configuration;
 using System.IO;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
 using Owin;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.OWIN;
-using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+using Microsoft.Identity.Web.TokenCacheProviders.Distributed;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Interop;
@@ -40,7 +41,19 @@ namespace LegacyWebApplication
             // Add the services you need.
             owinTokenAcquirerFactory.Services
                 .AddMicrosoftGraph()
-                .AddInMemoryTokenCaches();
+                .AddDistributedTokenCaches()
+                .AddDistributedSqlServerCache(options =>
+                {
+                    options.ConnectionString = ConfigurationManager.ConnectionStrings["Auth"].ConnectionString;
+                    options.SchemaName = "dbo";
+                    options.TableName = "TokenCache";
+
+                    // You don't want the SQL token cache to be purged before the access token has expired. Usually
+                    // access tokens expire after 1 hours (but this can be changed by token lifetime policies), whereas
+                    // the default sliding expiration for the distributed SQL database is 20 mins. 
+                    // Use a value which is above 60 mins (or the lifetime of a token in case of longer lived tokens)
+                    options.DefaultSlidingExpiration = TimeSpan.FromMinutes(90);
+                });
 
             owinTokenAcquirerFactory.Build();
         }
